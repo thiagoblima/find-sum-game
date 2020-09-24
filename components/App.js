409be0466bb56245e16861d0a8d0639450de99f2
Thiago Lima <thitheguy@gmail.com>
2020-09-24 00:38:21 -0300

    Adding Register Root Component

---
+++ components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class App extends React.Component {
+  render() {
+    return (
+        <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}
+
+export default App
