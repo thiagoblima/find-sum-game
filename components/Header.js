2355d5559ec89f9fcbdb8d6da0bf98d21483dbf2
Thiago Lima <thitheguy@gmail.com>
2020-09-24 03:35:50 -0300

    Adding Game Component Header & Stylings

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
     render() {
         return (
             <View>
-               <Text style={styles.header}> {this.props.messageTarget}  </Text>
+               <Text style={styles.header}> {this.props.messageTarget} </Text>
             </View>
         )
     }
