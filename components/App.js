e6f91b17378351966eeb12a7da0e25ad899ac8cc
Thiago Lima <thitheguy@gmail.com>
2020-09-24 01:01:22 -0300

    Adding Register Root Component

--- components/App.js
+++ components/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 class App extends React.Component {
   render() {
     return (
-        <View>
+        <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#ddd',
+        flex: 1
+    }
+})
 
 export default App
