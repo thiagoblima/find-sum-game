4d73c897b47b25e890b87b413189800859ab1a2c
Thiago Lima <thitheguy@gmail.com>
2020-09-24 03:04:22 -0300

    Adding Game Component Logic

--- components/Game.js
+++ components/Game.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Image} from 'react-native'
 import PropTypes from 'prop-types'
 
 class Game extends React.Component {
@@ -8,15 +8,20 @@ class Game extends React.Component {
     }
     randomNumbers = Array
         .from({length: this.props.randomNumberCount})
-        .map(() =>  1 + Math.floor(10 * Math.random()))
+        .map(() => 1 + Math.floor(10 * Math.random()))
     target = this.randomNumbers
         .slice(0, this.props.randomNumberCount - 2)
-        .reduce((acc, curr) => acc + curr , 0)
+        .reduce((acc, curr) => acc + curr, 0)
     render() {
         return (
             <View style={styles.container}>
+                <Text style={styles.header}> Target Sum Game </Text>
                 <Text style={styles.target}>{this.target}</Text>
-                <Text style={styles.target}>{this.randomNumbers}</Text>
+                <View style={styles.randomContainer}>
+                    {this.randomNumbers.map((randomNumber, i) =>
+                        <Text style={styles.random} key={i}> {randomNumber}</Text>
+                    )}
+                </View>
             </View>
         );
     }
@@ -25,14 +30,38 @@ class Game extends React.Component {
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#d2d2c9',
-        display: 'flex',
-        justifyContent: 'center',
-        flex: 1
+        flex: 1,
+    },
+    header: {
+        margin: 20,
+        fontSize: 30,
+        textAlign: 'center',
+        backgroundColor: '#6d031c',
+        color: '#d2d2c9',
+        padding: 25,
     },
     target: {
-        fontSize: 40,
+        fontSize: 50,
+        backgroundColor: '#673e37',
+        color: '#d2d2c9',
+        margin: 50,
+        textAlign: 'center'
+    },
+    randomContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around'
+    },
+    random: {
         backgroundColor: '#a79086',
-        marginHorizontal: 50,
+        color: '#d2d2c9',
+        fontSize: 35,
+        width: 100,
+        height: 100,
+        lineHeight: 100,
+        marginHorizontal: 15,
+        marginVertical: 35,
         textAlign: 'center'
     }
 })
